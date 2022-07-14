9dbfcca697e69ca50b1631add2feb971e610f989
carloseduneves <carlosneves.edu@gmail.com>
2022-07-13 21:28:35 -0300

    1.2

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
+'use strict'
+//Seleção de elementos
+const altura = document.getElementById('altura')
+const peso = document.getElementById('kilo')
+const resposta1 = document.getElementById('res1')
+const resposta2 = document.getElementById('res2')
+const resposta3 = document.getElementById('res3')
+const títulorespostas = document.querySelector('#titleanswer')
 
-
+//Interação da página
 function operar(){
-    var altura = document.getElementById('altura')
-    var peso = document.getElementById('kilo')
-    var resposta1 = document.getElementById('res1')
-    var resposta2 = document.getElementById('res2')
-    var resposta3 = document.getElementById('res3')
-    var títulorespostas = document.querySelector('#titleanswer')
-    var Altura = Number(altura.value)
-    var Peso = Number(peso.value)
-    var IMC = parseInt(Peso / (Altura ** 2))
-    var mínimo = Number.parseInt(18 * Altura ** 2) 
-    var máximo = Number.parseInt(24 * Altura ** 2)
+    //Lógica da calculadora
+    let Altura = Number(altura.value)
+    let Peso = Number(peso.value)
+    let IMC = parseInt(Peso / (Altura ** 2))
+
+    //Parâmetros para as condições
+    let mínimo = Number.parseInt(18 * Altura ** 2) 
+    let máximo = Number.parseInt(24 * Altura ** 2)
+    
+    //Respostas finais condicionais
     resposta1.innerText = `O seu índice de massa corporal é aproximadamente ${IMC} k/m².`
+
     títulorespostas.style.display = 'block'
+    
     if(IMC >= 18.5 && IMC <= 24){
         resposta2.innerText = `Parabéns! Isso é um bom sinal de que você está saudável e dentro da sua margem de peso ideal, no entanto, consulte um médico regularmente.`
     } else if(IMC <= 18 ){
